77f667bac9e04691ad5832f3eea0321a6206a018
SeyyedAmirHussein <148333552+SeyyedAmirHussein@users.noreply.github.com>
2023-11-09 21:36:02 +0330

    Create The Main Code OF The Project OF Finding The Character . .C

---
+++ The Main Code OF The Project OF Finding The Character . .C
@@ -0,0 +1,61 @@
+// The Project OF Finding The Character .
+
+// The Main Code OF The Project OF Finding The Character . .C
+
+// In this program, We want to receive a Character from the Serial Monitor, and if this Character is ' A ' , it prints the phrase " Character, A " on the Serial Monitor, otherwise the phrase " A Not Found " will be printed .
+
+
+
+
+void setup () {
+
+
+  Serial.begin (9600);                                                                                                                                // Introducing The Series .
+
+
+}
+
+
+void loop () {
+
+
+  if (Serial.available () > 0 ) {
+
+
+    char The Character = (char) Serial.read ();
+
+
+    if ( The Character == ' A ' ) {
+
+
+      Serial.print (" * * * * The Character : A * * * * ");                                                                           // Show The Desired Message .
+
+
+    }
+
+
+    else
+
+
+    {
+
+
+      Serial.println (" * * * * The Character : A Not Found * * * * ");                                                      // Show The Desired Message .
+
+
+    }
+
+
+}
+
+
+while (true)
+
+
+  {
+
+
+    }
+
+
+}
